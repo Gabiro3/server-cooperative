1cde569ad784f305b8bb4b194a590c08a23fa402
Gabiro Arnaud <gabiroarnaud@gmail.com>
2025-02-19 09:19:13 +0200

    Auth token error fix

--- src/index.ts
+++ src/index.ts
@@ -48,19 +48,6 @@ app.use(
   })
 );
 
-app.get(
-  `/`,
-  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException(
-      "This is a bad request",
-      ErrorCodeEnum.AUTH_INVALID_TOKEN
-    );
-    return res.status(HTTPSTATUS.OK).json({
-      message: "Hello Subscribe to the channel & share",
-    });
-  })
-);
-
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
