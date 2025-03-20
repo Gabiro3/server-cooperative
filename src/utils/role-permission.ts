9e895bd4c0a8868392a03fcdeed7cc3d8e9dd1cf
Gabiro Arnaud <gabiroarnaud@gmail.com>
2025-03-20 15:11:55 +0200

    Temp added officer roles

--- src/utils/role-permission.ts
+++ src/utils/role-permission.ts
@@ -36,7 +36,22 @@ export const RolePermissions: Record<RoleType, Array<PermissionType>> = {
     Permissions.VIEW_ONLY,
   ],
   OFFICER: [
-    Permissions.EDIT_TASK,
+    Permissions.CREATE_WORKSPACE,
+    Permissions.EDIT_WORKSPACE,
+    Permissions.DELETE_WORKSPACE,
+    Permissions.MANAGE_WORKSPACE_SETTINGS,
+
+    Permissions.ADD_MEMBER,
+    Permissions.CHANGE_MEMBER_ROLE,
+    Permissions.REMOVE_MEMBER,
+
+    Permissions.CREATE_PROJECT,
+    Permissions.EDIT_PROJECT,
+    Permissions.DELETE_PROJECT,
+
+    Permissions.CREATE_TASK,
+    Permissions.DELETE_TASK,
+
     Permissions.VIEW_ONLY,
   ],
   MEMBER: [
